447b4145b39f67c68dac370f5d110b0c37370442
JackMD <jackmtaylor.jmt@gmail.com>
2019-06-02 04:45:09 +0500

    new command /feed

---
+++ src/JackMD/Charm/Command/Defaults/Feed.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types = 1);
+
+namespace JackMD\Charm\Command\Defaults;
+
+use JackMD\Charm\Charm;
+use JackMD\Charm\Command\BaseCommand;
+use JackMD\Charm\Utils\PlayerUtils;
+use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\level\particle\HappyVillagerParticle;
+use function strtolower;
+
+class Feed extends BaseCommand{
+
+	/**
+	 * Feed constructor.
+	 *
+	 * @param Charm $plugin
+	 */
+	public function __construct(Charm $plugin){
+		parent::__construct(
+			$plugin,
+
+			"feed",
+			"charm.command.feed.use",
+			"Feed yourself or another player.",
+			"/feed [string:player]"
+		);
+	}
+
+	/**
+	 * @param CommandSender $sender
+	 * @param string        $label
+	 * @param array         $args
+	 */
+	public function onCommand(CommandSender $sender, string $label, array $args): void{
+		$target = $sender;
+
+		if(isset($args[0])){
+			$targetName = strtolower($args[0]);
+			$target = $this->getServer()->getPlayer($targetName);
+
+			if(!PlayerUtils::isOnline($target)){
+				$this->sendError($sender, "Player §4$targetName §cis not online or doesn't exist.");
+
+				return;
+			}
+		}
+
+		if($target instanceof ConsoleCommandSender){
+			$this->sendError($sender, "Please provide a player to heal.");
+			$this->sendError($sender, "Usage: " . $this->getUsage());
+
+			return;
+		}
+
+		$target->setFood(20);
+		$target->getLevel()->addParticle(new HappyVillagerParticle($target->add(0, 2)));
+
+		$this->sendMessage($target, "You have been fed.");
+
+		if($target !== $sender){
+			$this->sendMessage($sender, "Player §2{$target->getName()} §ahas been fed.");
+		}
+	}
+}
